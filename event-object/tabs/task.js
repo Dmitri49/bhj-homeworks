48627d9bf7cb089701246b71e741c857ecd4f749
Erika-Michel <erikamichel174@gmail.com>
2023-05-25 19:04:38 +0200

    commit

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,19 +1,21 @@
-const tabElements = Array.from(document.querySelectorAll('.tab'));
-const tabConten = Array.from(document.querySelectorAll('.tab__content'));
+const tabs = Array.from(document.querySelectorAll('.tab'));
+const tabContents = document.querySelectorAll('.tab__content');
 
-tabElements.forEach((item) => {
+tabs.forEach(onClickTabs);
+
+function onClickTabs(item) {
     item.addEventListener('click', () => {
-        tabElements.forEach((elem) => {elem.classList.remove('tab_active')});
-        item.classList.add('tab_active');
-
-        let index = tabElements.indexOf(item);
-
-        let activeContent = document.querySelector('.tab__content_active');
-
-        activeContent.classList.remove('tab__content_active');
-        tabConten[index].classList.add('tab__content_active')
-    });
-})
+        if (!item.classList.contains('tab_active')) {
+            tabs.forEach((item) => {item.classList.remove('tab_active')});
+            item.classList.add('tab_active');
+    
+            let index = tabs.indexOf(item);
+            
+            tabContents.forEach((item) => {item.classList.remove('tab__content_active')});
+            tabContents[index].classList.add('tab__content_active');
+        }
+    })
+}
 
 
 
